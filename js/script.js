22b4fd7348f7ac728477db906d13d44b8bb2cb59
Avaz <elonarm21@gmail.com>
2024-01-15 00:48:19 +0500

    loop

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
-let number = 10
-const borderColor = "red"
-console.log(number)
+"use strict"
+let numberOfSeries = +prompt("Nechta serial korgansiz?")
+
+const seriesDB = {
+    count: numberOfSeries,
+    series: {},
+    genres: [],
+    private: false
+}
+
+if (seriesDB.count >= 10) {
+    console.log("Eng kop")
+} else if (5 <= seriesDB.count && seriesDB.count < 10) {
+    console.log("Normal")
+} else {
+    console.log("Eng kam")
+}
+
+for (let i = 0; i < 2; i++) {
+    const serialTitle = prompt("Serial nomi?"),
+        serialWatchCount = prompt("Necha marotaba korgansiz?")
+
+    if (serialTitle != null && serialWatchCount != null && serialTitle !== "" && serialWatchCount !== "") {
+        seriesDB.series[serialTitle] = serialWatchCount
+        console.log("done")
+    } else {
+        console.log("error")
+        i--
+    }
+}
+console.log("seriesDB => ", seriesDB)
+//Todo: for ni orniga while, do...while bilan ham qilish kerak.
