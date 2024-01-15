4411951d710b030e3cec6ad47457119b95127a15
Avaz <elonarm21@gmail.com>
2024-01-15 12:42:08 +0500

    js practice done

--- js/script.js
+++ js/script.js
@@ -3,18 +3,45 @@
 let numberOfSeries
 
 function startApp() {
-    numberOfSeries = +prompt("Necha marta serial korgansiz?")
-    while (numberOfSeries === 0 || numberOfSeries === "" || numberOfSeries === null) {
+    do {
         numberOfSeries = +prompt("Necha marta serial korgansiz?")
-    }
+    } while (numberOfSeries === 0)
 }
 
 startApp()
 
-const series = {
+const seriesDB = {
     count: numberOfSeries,
     genres: [],
     series: {},
 }
 
-console.log(series)
+function seriesFunc() {
+    let serialTitle, serialMark;
+    let i = 0
+    do {
+        serialTitle = prompt("Serial title?")
+        serialMark = prompt("Serial mark?")
+        if (serialTitle !== "" && serialTitle !== null && serialMark !== "" && serialMark !== null) {
+            seriesDB.series[serialTitle] = serialMark
+            i++
+        } else {
+            i = 0
+        }
+    } while (i < 2)
+}
+
+seriesFunc()
+
+function countFunc() {
+    if (seriesDB.count >= 10) {
+        console.log("Max viewer")
+    } else if (5 <= seriesDB.count && seriesDB.count < 10) {
+        console.log("Normal viewer")
+    } else {
+        console.log("Min viewer")
+    }
+}
+
+countFunc()
+console.log(seriesDB)
